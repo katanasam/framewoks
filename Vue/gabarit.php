7a62874ff0737da8ee714ffd9d96094a271826cf
unknown <katana.kenzo@gmail.com>
2019-09-06 14:44:37 +0200

    automatisé le controller central

--- Vue/gabarit.php
+++ Vue/gabarit.php
@@ -17,7 +17,7 @@
         <footer id="piedBlog">
                 Blog réalisé avec PHP, HTML5 et CSS.
         </footer>
-        </div> <!-- #global -->
+        </div> <!--#global -->
     </body>
 </html>
 
